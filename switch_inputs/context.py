a92a8454bfa99655be22f6213207033941073d52
pesap <pesapsanchez@gmail.com>
2018-06-05 11:13:23 -0700

    Fixed path error

--- switch_inputs/context.py
+++ switch_inputs/context.py
@@ -6,6 +6,9 @@ script_path = os.path.dirname(__file__)
 parent_path = os.path.dirname(os.path.dirname(__file__))
 data_path = os.path.join(parent_path, 'data/clean/loads')
 default_path = os.path.join(parent_path, 'data/default/')
-default_path = Path('data/default/')
+default_path = str(Path('data/default/')) + '/'
 output_path  = os.path.join(parent_path, 'data/switch_inputs/')
-output_path  = Path('data/switch_inputs/')
+output_path  = str(Path('data/switch_inputs/')) + '/'
+
+if __name__ == "__main__":
+    print (default_path, output_path)
