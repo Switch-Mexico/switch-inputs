8bf436c16a1b418d0922f5f69466d3380bf60d92
pesap <pesapsanchez@gmail.com>
2018-06-05 10:20:30 -0700

    Fixed error with path. Changed it to Pathlib.

--- switch_inputs/context.py
+++ switch_inputs/context.py
@@ -1,8 +1,11 @@
 import os
 import sys
+from pathlib import Path
 
 script_path = os.path.dirname(__file__)
 parent_path = os.path.dirname(os.path.dirname(__file__))
 data_path = os.path.join(parent_path, 'data/clean/loads')
 default_path = os.path.join(parent_path, 'data/default/')
+default_path = Path('data/default/')
 output_path  = os.path.join(parent_path, 'data/switch_inputs/')
+output_path  = Path('data/switch_inputs/')
